cbd8d549bad3a9d85703334363e0c3ea39685841
diackster <diackster@gmail.com>
2019-09-17 12:53:25 +0300

    с мультипроцессингом

--- problem 280.py
+++ problem 280.py
@@ -1,41 +1,44 @@
 import random
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from multiprocessing import Process, Manager
+from datetime import datetime, timedelta
+
 
 class grid:
     def __init__(self):
-        self.upper = [False,False,False,False,False]
-        self.lower = [True,True,True,True,True]
-        
+        self.upper = [False, False, False, False, False]
+        self.lower = [True, True, True, True, True]
+
 
 class ant:
     def __init__(self):
-        self.position = [2,2]
+        self.position = [2, 2]
         self.seed = False
         self.moves = 0
-        
+
     def get_move(self):
         if self.position[0] == 0 and self.position[1] == 0:
-            move = random.choice(['R','D'])
+            move = random.choice(['R', 'D'])
         elif self.position[0] == 4 and self.position[1] == 0:
-            move = random.choice(['U','R'])
+            move = random.choice(['U', 'R'])
         elif self.position[0] == 0 and self.position[1] == 4:
-            move = random.choice(['L','D'])
+            move = random.choice(['L', 'D'])
         elif self.position[0] == 4 and self.position[1] == 4:
-            move = random.choice(['U','L'])
+            move = random.choice(['U', 'L'])
         elif self.position[0] == 0:
-            move = random.choice(['L','R','D'])
+            move = random.choice(['L', 'R', 'D'])
         elif self.position[1] == 0:
-            move = random.choice(['U','R','D'])
+            move = random.choice(['U', 'R', 'D'])
         elif self.position[0] == 4:
-            move = random.choice(['L','U','R'])
+            move = random.choice(['L', 'U', 'R'])
         elif self.position[1] == 4:
-            move = random.choice(['U','L','D'])
+            move = random.choice(['U', 'L', 'D'])
         else:
-            move = random.choice(['U','R','D','L'])
-        
+            move = random.choice(['U', 'R', 'D', 'L'])
+
         return move
-    
+
     def move(self):
         move = self.get_move()
         if move == 'U':
@@ -46,16 +49,15 @@ class ant:
             self.position[0] += 1
         if move == 'L':
             self.position[1] -= 1
-            
+
         self.moves += 1
 
 
 def ant_run(ant, grid):
-    
     while False in grid.upper:
 
         ant.move()
-        
+
         if ant.position[0] == 4 and ant.seed == False and grid.lower[ant.position[1]] == True:
             ant.seed = True
             grid.lower[ant.position[1]] = False
@@ -66,13 +68,41 @@ def ant_run(ant, grid):
 
     return ant.moves
 
-moves_list = []
 
-for i in range(30000000):
-    grid1 = grid()
-    ant1 = ant()
-    moves_list.append(ant_run(ant1, grid1))
-    if len(moves_list)%10000 == 0:
-        print(f'Move number: {len(moves_list)}, mean: {np.mean(moves_list)}')    
-    
-print(np.mean(moves_list))
+def ant_runner(l, num):
+    for i in range(num):
+        grid1 = grid()
+        ant1 = ant()
+        l.append(ant_run(ant1, grid1))
+
+
+#        if len(l) % 1000 == 0:
+#            print(f'Move number: {len(l)}, mean: {np.mean(l)}')
+
+
+L = []
+df_speed = 3000
+
+def run(procs_num, runs):
+    if __name__ == "__main__":
+        with Manager() as manager:
+            L = manager.list()  # <-- can be shared between processes.
+            processes = []
+            start = datetime.now()
+            plan = start + timedelta(0,procs_num*runs/df_speed)
+            print(f'Launched {procs_num} processes at {start.time()} for {runs} each. Planned end: {plan.time()}')
+            for i in range(procs_num):
+                p = Process(target=ant_runner, args=(L, runs))  # Passing the list
+                p.start()
+                processes.append(p)
+            for p in processes:
+                p.join()
+            end = datetime.now()
+            delta = end - start
+#            print(delta.seconds)
+#            print(len(L))
+            print(f'Finished {len(L)/delta.seconds} iter/sec at {end.time()} Mean value: {np.mean(L)}')
+
+#run(1,15000)
+#run(2,7500)
+run(4,10000)
