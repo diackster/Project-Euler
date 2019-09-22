328168036e1cf97acfcca0961f2534d8fd1d701a
diackster <diackster@gmail.com>
2019-09-22 15:49:01 +0300

    Чужое решение

---
+++ problem 1.py
@@ -0,0 +1,9 @@
+import numpy as np
+def find_multiples(n):
+    multiples = []
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            multiples.append(i)
+    return np.sum(multiples)
+
+print(find_multiples(1000))
