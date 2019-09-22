0385114baba0a44310edef1828d3400b75d0f10d
diackster <diackster@gmail.com>
2019-09-22 15:34:21 +0300

    Готово

---
+++ problem 151b.py
@@ -0,0 +1,32 @@
+def cut(sheet):
+    if sheet == 1:
+        return ()
+    if sheet > 1:
+        tempsheets = []
+        while sheet > 1:
+            tempsheets.append(sheet/2)
+            sheet /= 2
+        return tuple(tempsheets)
+
+variants = {}
+
+def get_single_num(a):
+    result = 0.0
+    b = tuple(a)
+    if b not in variants:
+        if len(b) > 0:
+            for i in range(len(b)):
+                sheet = b[i]
+                tempsheets = list(b)
+                del tempsheets[i]
+                tempsheets += cut(sheet)
+                tempsheets.sort()
+                result += get_single_num(tuple(tempsheets))
+            result /= len(b)
+            if len(b) == 1:
+                result += 1.0
+        variants[b] = result
+#    print(vars[a])
+    return variants[b]
+
+print(get_single_num((16,))-2)
